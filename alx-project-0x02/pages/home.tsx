662f3c122f1d49f1c6a4d12e47ec4ee4bbd6372e
Dibz.Dev <tommwaduka@gmail.com>
2025-11-21 14:33:58 +0300

    Fix Task 3: Correct Card component file name case

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,5 +1,5 @@
 import Header from '../components/layout/Header';
-import Card from '../components/common/Card.tsx';
+import Card from '../components/common/card';
 
 export default function HomePage() {
   return (
