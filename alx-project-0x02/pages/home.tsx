f5ef9aa4794b642eac50962f1737628224a29f92
Dibz.Dev <tommwaduka@gmail.com>
2025-11-21 14:29:25 +0300

    Fix Task 3: Correct Card component implementation

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,5 +1,5 @@
 import Header from '../components/layout/Header';
-import Card from '../components/common/Card';
+import Card from '../components/common/card';
 
 export default function HomePage() {
   return (
